d347b5a2833cf23f525d6c628d7f1c48a6ce74eb
RuthwikGade <gaderuthwik110@gmail.com>
2025-12-27 14:24:00 +0530

    Clean up database connection configuration
    Removed commented dotenv and hardcoded database connection string.

--- Backend/config/DataBaseConn.js
+++ Backend/config/DataBaseConn.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
-// require('dotenv').config();
 const DbString = process.env.DbString
-// const DbString = "mongodb+srv://TaskManager:ruthwik123@cluster0.jhtn9tu.mongodb.net/?appName=Cluster0"
 
 
 const Connectdb = async () =>{
